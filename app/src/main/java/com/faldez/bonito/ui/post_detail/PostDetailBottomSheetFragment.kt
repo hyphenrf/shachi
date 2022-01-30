0a813799335f9583c660f89365ed4a398cfe9767
faldez <github@faldez.dev>
2022-01-30 17:04:45 +0700

    simplify post detail fragment

--- app/src/main/java/com/faldez/bonito/ui/post_detail/PostDetailBottomSheetFragment.kt
+++ app/src/main/java/com/faldez/bonito/ui/post_detail/PostDetailBottomSheetFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
@@ -19,6 +20,7 @@ import com.faldez.bonito.model.Tag
 import com.faldez.bonito.service.BooruService
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -38,28 +40,27 @@ class PostDetailBottomSheetFragment : BottomSheetDialogFragment() {
         val server = requireArguments().get("server") as Server?
         val post = requireArguments().get("post") as Post?
 
-        val test = requireArguments().get("tags") as List<Tag>?
-        Log.d("PostDetailBottomSheetFragment", "currentSearchTags $test")
-        val currentSearchTags = test?.map { it.name }?.toSet()
+        val currentSearchTags = requireArguments().get("tags") as List<Tag>?
+        Log.d("PostDetailBottomSheetFragment", "currentSearchTags $currentSearchTags")
+        val currentSearchTagsSet = currentSearchTags?.map { it.name }?.toSet()
 
         viewModel = PostDetailBottomSheetViewModel(server, TagRepository(BooruService()))
 
-        post?.let { post ->
-            binding.sizeTextview.text = "${post.width}x${post.height}"
-            binding.sourceUrl.text = post?.source
-            binding.ratingTextview.text = post.rating
-            binding.scoreTextview.text = "${post.score ?: 0}"
-            binding.postedTextview.text = post.createdAt
+        post?.let { p ->
+            binding.sizeTextview.text = "${p.width}x${p.height}"
+            binding.sourceUrl.text = p.source
+            binding.ratingTextview.text = p.rating
+            binding.scoreTextview.text = "${p.score ?: 0}"
+            binding.postedTextview.text = p.createdAt
 
-            viewModel.getTags(post.tags)
+            viewModel.getTags(p.tags)
         }
 
         lifecycleScope.launch {
             viewModel.state.collect { tags ->
                 binding.loadingLabel.isVisible = tags == null
                 val splitTags =
-                    (tags ?: listOf()).groupBy { currentSearchTags?.contains(it.name) ?: false }
-                val currentSearchGroupedTags = splitTags[true]?.groupBy { it.type }
+                    (tags ?: listOf()).groupBy { currentSearchTagsSet?.contains(it.name) ?: false }
                 val groupedTags = splitTags[false]?.groupBy { it.type }
                 Log.d("SearchSimpleFragment", "collect $groupedTags")
                 binding.currentSearchTagsHeader.isVisible = false
@@ -70,52 +71,63 @@ class PostDetailBottomSheetFragment : BottomSheetDialogFragment() {
                 tagDetailsBinding.metadataTagsHeader.isVisible = false
                 tagDetailsBinding.otherTagsHeader.isVisible = false
 
-                currentSearchGroupedTags?.forEach { (type, tags) ->
-                    var group = binding.currentSearchTagsChipGroup
-                    var header = binding.currentSearchTagsHeader
-                    group.removeAllViews()
-                    header.isVisible = tags.isNotEmpty()
-                    group.isVisible = tags.isNotEmpty()
-
-                    tags.forEach { tag ->
-                        val chip = Chip(requireContext())
-                        chip.bindCurrentSearch(tag)
+                binding.currentSearchTagsHeader.isVisible = !splitTags[true].isNullOrEmpty()
+                binding.currentSearchTagsChipGroup.isVisible = !splitTags[true].isNullOrEmpty()
+                splitTags[true]?.sortedBy { it.type }?.forEach { tag ->
+                    val textColor = when (tag.type) {
+                        0 -> R.color.tag_general
+                        1 -> R.color.tag_artist
+                        3 -> R.color.tag_copyright
+                        4 -> R.color.tag_character
+                        5 -> R.color.tag_metadata
+                        else -> null
                     }
+
+                    val chip = Chip(requireContext())
+                    chip.bind(binding.currentSearchTagsChipGroup, textColor, tag)
                 }
 
                 groupedTags?.forEach { (type, tags) ->
-                    var group = tagDetailsBinding.otherTagsChipGroup
-                    var header = tagDetailsBinding.otherTagsHeader
+                    lateinit var group: ChipGroup
+                    lateinit var header: TextView
+                    var textColor: Int? = null
                     when (type) {
                         0 -> {
                             group = tagDetailsBinding.generalTagsChipGroup
                             header = tagDetailsBinding.generalTagsHeader
+                            textColor = R.color.tag_general
                         }
                         1 -> {
                             group = tagDetailsBinding.artistTagsChipGroup
                             header = tagDetailsBinding.artistTagsHeader
+                            textColor = R.color.tag_artist
                         }
                         3 -> {
                             group = tagDetailsBinding.copyrightTagsChipGroup
                             header = tagDetailsBinding.copyrightTagsHeader
+                            textColor = R.color.tag_copyright
                         }
                         4 -> {
                             group = tagDetailsBinding.characterTagsChipGroup
                             header = tagDetailsBinding.characterTagsHeader
+                            textColor = R.color.tag_character
                         }
                         5 -> {
                             group = tagDetailsBinding.metadataTagsChipGroup
                             header = tagDetailsBinding.metadataTagsHeader
+                            textColor = R.color.tag_metadata
                         }
-                        else -> tagDetailsBinding.otherTagsChipGroup
+                        else -> {
+                            group = tagDetailsBinding.otherTagsChipGroup
+                            header = tagDetailsBinding.otherTagsHeader
+                        }
                     }
-                    group.removeAllViews()
                     header.isVisible = tags.isNotEmpty()
                     group.isVisible = tags.isNotEmpty()
 
                     tags.forEach { tag ->
                         val chip = Chip(requireContext())
-                        chip.bind(tag)
+                        chip.bind(group, textColor, tag)
                     }
                 }
             }
@@ -124,66 +136,8 @@ class PostDetailBottomSheetFragment : BottomSheetDialogFragment() {
         return binding.root
     }
 
-    private fun Chip.bindCurrentSearch(tag: Tag) {
-        var textColor: Int? = null
-        var group = binding.currentSearchTagsChipGroup
-        when (tag.type) {
-            0 -> {
-                textColor = R.color.tag_general
-            }
-            1 -> {
-                textColor = R.color.tag_artist
-            }
-            3 -> {
-                textColor = R.color.tag_copyright
-            }
-            4 -> {
-                textColor = R.color.tag_character
-            }
-            5 -> {
-                textColor = R.color.tag_metadata
-            }
-        }
-
-        this.apply {
-            text = tag.name
-            textColor?.let {
-                setTextColor(ColorStateList.valueOf(ResourcesCompat.getColor(resources,
-                    textColor,
-                    requireActivity().theme)))
-            }
-            setEnsureMinTouchTargetSize(false)
-            group.addView(this)
-        }
-    }
-
-
-    private fun Chip.bind(tag: Tag) {
+    private fun Chip.bind(group: ChipGroup, textColor: Int?, tag: Tag) {
         Log.d("PostDetailBottomSheetFragment", "$tag")
-        var textColor: Int? = null
-        var group = tagDetailsBinding.otherTagsChipGroup
-        when (tag.type) {
-            0 -> {
-                textColor = R.color.tag_general
-                group = tagDetailsBinding.generalTagsChipGroup
-            }
-            1 -> {
-                textColor = R.color.tag_artist
-                group = tagDetailsBinding.artistTagsChipGroup
-            }
-            3 -> {
-                textColor = R.color.tag_copyright
-                group = tagDetailsBinding.copyrightTagsChipGroup
-            }
-            4 -> {
-                textColor = R.color.tag_character
-                group = tagDetailsBinding.characterTagsChipGroup
-            }
-            5 -> {
-                textColor = R.color.tag_metadata
-                group = tagDetailsBinding.metadataTagsChipGroup
-            }
-        }
 
         this.apply {
             text = tag.name
