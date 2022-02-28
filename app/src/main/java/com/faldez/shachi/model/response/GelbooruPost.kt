6e078f47f813957868e6fab65d499333dfa6d410
faldez <github@faldez.dev>
2022-02-28 22:36:47 +0700

    if sample_url or preview_url is empty, set as null

--- app/src/main/java/com/faldez/shachi/model/response/GelbooruPost.kt
+++ app/src/main/java/com/faldez/shachi/model/response/GelbooruPost.kt
@@ -4,13 +4,10 @@ import android.os.Parcelable
 import com.faldez.shachi.util.type_adapter.EmptyStringAsNullTypeAdapter
 import com.faldez.shachi.util.type_adapter.SingleObjectAsArrayTypeAdapter
 import com.faldez.shachi.util.type_adapter.ZonedDateTimeAdapter
-import com.google.gson.*
 import com.google.gson.annotations.JsonAdapter
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
-import java.lang.reflect.Type
 import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 
 data class GelbooruPostResponse(
     val posts: GelbooruPosts?,
@@ -30,10 +27,10 @@ data class GelbooruPost(
     @JsonAdapter(EmptyStringAsNullTypeAdapter::class) val score: Int?,
     @SerializedName("file_url") val fileUrl: String,
     @JsonAdapter(EmptyStringAsNullTypeAdapter::class) @SerializedName("parent_id") val parentId: Int?,
-    @SerializedName("sample_url") val sampleUrl: String?,
+    @JsonAdapter(EmptyStringAsNullTypeAdapter::class) @SerializedName("sample_url") val sampleUrl: String?,
     @SerializedName("sample_width") val sampleWidth: Int?,
     @SerializedName("sample_height") val sampleHeight: Int?,
-    @SerializedName("preview_url") val previewUrl: String?,
+    @JsonAdapter(EmptyStringAsNullTypeAdapter::class) @SerializedName("preview_url") val previewUrl: String?,
     @SerializedName("preview_width") val previewWidth: Int?,
     @SerializedName("preview_height") val previewHeight: Int?,
     val rating: String,
