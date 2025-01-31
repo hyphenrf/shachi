abbd01152ee781fdc60a8e0a81e1e09d542c064d
Haz <hyphens@pm.me>
2025-01-31 19:48:28 +0200

    Don't crash on malformed-xml either
    A commenter had a quote in their name. Gelbooru returned that as
    backslash-escaped quote '\"' in the XML, rather than '&quot;'

--- app/src/main/java/com/hyphenrf/shachi/data/util/interceptor/XmlToJson.kt
+++ app/src/main/java/com/hyphenrf/shachi/data/util/interceptor/XmlToJson.kt
@@ -2,31 +2,44 @@ package com.hyphenrf.shachi.data.util.interceptor
 
 import android.util.Log
 import okhttp3.Interceptor
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.Response
-import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.json.jsonjava.JSONException
+import org.json.jsonjava.JSONStringer
 import org.json.jsonjava.XML
 
+private const val TAG = "TransformInterceptor"
 
 // This interceptor is only ever used for XML API responses, non-xml responses are errors, and
 // should not silently pass through.
 class XmlToJsonInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val response = chain.proceed(chain.request())
-        var body = response.body
-        val converted = if (body?.contentType()?.subtype == "xml") {
-            val json = XML.toJSONObject(body.string())
-            Log.d("TransformInterceptor", json.toString())
-            json.toString()
+        // XXX: can we ever hit a null case? says the body will be null if it's returned from
+        //      one of: `priorResponse`, `networkResponse`, `cacheResponse`
+        val body = response.body!!
+        val contents = body.string() // consume the body and close the connection
+        var json: String
+
+        if (body.contentType()?.subtype == "xml") try {
+            json = XML.toJSONObject(contents).toString()
+            Log.d(TAG, json)
+        } catch (e: JSONException) {
+            Log.e(TAG, "!!! MALFORMED XML !!! -- ${JSONStringer.valueToString(contents)}", e)
+            // TODO: find out if there are more malformed XML instances than '\" inside attr value'
+            // TODO: be more efficient in the way you recover, don't repeat parsing work
+            json = XML.toJSONObject(contents.replace("\\\"", "&quot;")).toString()
         } else {
-            val str = body?.string() ?: "No Response"
-            Log.e("TransformInterceptor", "NOT XML: $str")
-            """{"error": "$str"}"""
+            json = """{"error":{"type":"NoXML","detail":${JSONStringer.valueToString(contents)}}}"""
+            Log.e(TAG, "!!! NOT XML !!! -- $json")
         }
-        body = ResponseBody.create("application/json".toMediaTypeOrNull(), converted)
-        val builder = response.newBuilder()
-        return builder.header("Content-Type", "application/json").headers(response.headers)
-            .body(body).build()
+
+        return response.newBuilder()
+            .headers(response.headers)
+            .header("Content-Type", "application/json")
+            .body(json.toResponseBody(contentType = "application/json".toMediaType()))
+            .build()
     }
 
 }
