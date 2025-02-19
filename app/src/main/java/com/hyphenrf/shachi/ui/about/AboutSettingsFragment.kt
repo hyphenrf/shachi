4f4f7a63f2fe0e9fe87acc34edef41d0edae0e1a
Haz <hyphens@pm.me>
2025-02-19 04:58:57 +0200

    Add license notice in About
    Also remove some PNGs from resources

--- app/src/main/java/com/hyphenrf/shachi/ui/about/AboutSettingsFragment.kt
+++ app/src/main/java/com/hyphenrf/shachi/ui/about/AboutSettingsFragment.kt
@@ -3,6 +3,7 @@ package com.hyphenrf.shachi.ui.about
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.hyphenrf.shachi.R
@@ -12,15 +13,20 @@ class AboutSettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.about_preferences, rootKey)
         findPreference<Preference>(ShachiPreference.KEY_APP_VERSION)?.apply {
-            summary =
-                "v" + requireContext().packageManager.getPackageInfo(requireContext().packageName,
-                    0).versionName
+            val version = @Suppress("DEPRECATION")
+                context.packageManager.getPackageInfo(context.packageName, 0).versionName
+            summary = "v${version} (GPL-3.0)"
+            setOnPreferenceClickListener {
+                AlertDialog.Builder(context)
+                    .setMessage(getString(R.string.license_notice).trimIndent())
+                    .setPositiveButton(R.string.license_agree, null)
+                    .show()
+                true
+            }
         }
 
         findPreference<Preference>(ShachiPreference.KEY_GITHUB)?.setOnPreferenceClickListener {
-            val intent =
-                Intent(Intent.ACTION_VIEW,
-                    Uri.parse("https://github.com/hyphenrf/shachi"))
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/hyphenrf/shachi"))
             startActivity(intent)
             true
         }
